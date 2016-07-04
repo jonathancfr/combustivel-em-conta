61f7e0f5250c57c2a01a1e8652d94b17c24fda44
Ana Paula <apso0101@gmail.com>
2016-07-04 01:31:08 -0300

    Ícones adicionados/ adicionando padrões

--- app/src/main/java/br/inf/combustivelemconta/fragments/MapsFragment.java
+++ app/src/main/java/br/inf/combustivelemconta/fragments/MapsFragment.java
@@ -1,4 +1,4 @@
-package br.inf.combustivelemconta.activities;
+package br.inf.combustivelemconta.fragments;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -12,30 +12,25 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import br.inf.combustivelemconta.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsFragment extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+
+        initView();
+    }
+
+    private void initView() {
+        setContentView(R.layout.fragment_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
